b5448a049b192d25f8397063e1df315771d2127e
techforceconsulting <techforce220@zohomail.in>
2026-03-30 10:39:02 +0530

    created register api and disabled security

--- src/main/java/com/techforce/assesment/Repository/UserRepository.java
+++ src/main/java/com/techforce/assesment/Repository/UserRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.techforce.assesment.Model.User;
 import java.util.Optional;
 
-public interface userRepository extends JpaRepository<User, String> {
-    Optional<User> findByEmail(String email);
-    Boolean existsByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
