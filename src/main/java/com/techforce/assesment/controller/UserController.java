985371c1a362723671db9fb0904b8de77ef84eb1
techforceconsulting <techforce220@zohomail.in>
2026-03-30 14:42:13 +0530

    register changes

--- src/main/java/com/techforce/assesment/controller/UserController.java
+++ src/main/java/com/techforce/assesment/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.techforce.assesment.controller;
 
 import com.techforce.assesment.Model.User;
-
-import com.techforce.assesment.Repository.UserRepository;
+import com.techforce.assesment.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    private final UserRepository repo;
 
-    public UserController(UserRepository repo) {
-        this.repo = repo;
-    }
+    @Autowired
+    private UserService userService;
 
     @PostMapping("/register")
-    public String createUser(@RequestBody User user){
-        repo.save(user);
-        return "User registered successfully";
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User registeredUser = userService.registerUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(registeredUser);
     }
 }
---
+++ src/main/java/com/techforce/assesment/service/UserService.java
@@ -0,0 +1,7 @@
+package com.techforce.assesment.service;
+
+import com.techforce.assesment.Model.User;
+
+public interface UserService {
+    User registerUser(User user);
+}
